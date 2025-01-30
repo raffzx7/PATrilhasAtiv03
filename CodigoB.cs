66320ed0e86355c6163a1baebc7d7ccc0ab6ef81
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2025-01-30 09:08:25 -0300

    Digitar dois números inteiros e exibir se o primeiro número é menor que o segundo número.

---
+++ CodigoB.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    	Console.WriteLine("Insira um numero inteiro: ");
+    	int n1 = int.Parse(Console.ReadLine());
+    	Console.WriteLine("Insira outro numero inteiro: ");
+    	int n2 = int.Parse(Console.ReadLine());
+    	
+    	if (n1 < n2) {
+        	Console.WriteLine("{0} é menor que {1}", n1, n2);
+        }   
+    	Console.WriteLine("");
+    	Console.ReadKey(true);
+    }
+  }
+}
